200a021a7448b602e050e894318b4473c1e600e9
Adish Antony <adishantony@gmail.com>
2019-01-24 22:35:43 +0530

    Added shop components

--- bill-creator-frontend/src/app/app.module.ts
+++ bill-creator-frontend/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ListShopComponent } from './list-shop/list-shop.component';
+import { CreateShopComponent } from './create-shop/create-shop.component';
+import { UpdateShopComponent } from './update-shop/update-shop.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListShopComponent,
+    CreateShopComponent,
+    UpdateShopComponent
   ],
   imports: [
     BrowserModule
